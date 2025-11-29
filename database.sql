f751bd8db221cf6bd9365bf4c1952bbd2a2120f0
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-28 21:21:20 -0400

    bugfix: Right database add code

--- database.sql
+++ database.sql
@@ -1,9 +1,14 @@
-CREATE DATABASE product_db;
+-- Create the database
+CREATE DATABASE IF NOT EXISTS product_db;
 
+-- Use the database we just created
+USE product_db;
+
+-- Create the products table
 CREATE TABLE product (
- id INT NOT NULL AUTO_INCREMENT,
- name VARCHAR(128) NOT NULL,
- size INT NOT NULL DEFAULT 0,
- is_available BOOLEAN NOT NULL DEFAULT FALSE,
- PRIMARY KEY (id)
-);
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    size INT NOT NULL DEFAULT 0,
+    is_available BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
